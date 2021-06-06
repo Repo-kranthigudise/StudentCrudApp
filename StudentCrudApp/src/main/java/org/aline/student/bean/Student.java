a2be405603780977a9948d6c0bb08d3d1656833e
Kranthi G <Kranthi G@KranthiG-PC>
2021-06-06 00:15:23 -0700

    crating standard packeges and student bean class

---
+++ StudentCrudApp/src/main/java/org/aline/student/bean/Student.java
@@ -0,0 +1,57 @@
+package org.aline.student.bean;
+
+public class Student {
+	private int sId;
+	private String fName;
+	private String lName;
+	private String branch;
+	private double totalMarks;
+	
+	public Student()
+	{	
+		
+	}
+	public Student(int sId, String fName, String lName, String branch, double totalMarks) {
+		super();
+		
+		this.sId = sId;
+		this.fName = fName;
+		this.lName = lName;
+		this.branch = branch;
+		this.totalMarks = totalMarks;
+	}
+	
+	
+	public int getsId() {
+		return sId;
+	}
+	public void setsId(int sId) {
+		this.sId = sId;
+	}
+	
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public double getTotalMarks() {
+		return totalMarks;
+	}
+	public void setTotalMarks(double totalMarks) {
+		this.totalMarks = totalMarks;
+	}
+	
+}
